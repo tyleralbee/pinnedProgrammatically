2bb8c139e4e0bb13f8a2e88a62235d66b169364c
tyleralbee <tyal7988@colorado.edu>
2019-08-22 19:14:16 -0700

    Initial Commit

---
+++ pinnedProgrammatically/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pinnedProgrammatically
+//
+//  Created by Tyler Albee on 8/22/19.
+//  Copyright © 2019 Tyler Albee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ pinnedProgrammaticallyTests/pinnedProgrammaticallyTests.swift
@@ -0,0 +1,34 @@
+//
+//  pinnedProgrammaticallyTests.swift
+//  pinnedProgrammaticallyTests
+//
+//  Created by Tyler Albee on 8/22/19.
+//  Copyright © 2019 Tyler Albee. All rights reserved.
+//
+
+import XCTest
+@testable import pinnedProgrammatically
+
+class pinnedProgrammaticallyTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
